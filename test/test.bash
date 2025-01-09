8400f100e6fcb9421f6d574202b21f7cc87dc102
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-10 03:07:47 +0900

    add

--- test/test.bash
+++ test/test.bash
@@ -5,10 +5,10 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd "$dir/ros2_ws" || { echo "ディレクトリが見つかりません: $dir/ros2_ws"; exit 1; }
+cd "$dir/ros2_ws"
 source "$dir/ros2_ws/install/setup.bash"
 
 timeout 10 ros2 run ros2mypkg baito_publisher > /tmp/ros2mypkg.log
 
-grep -e '経過時間' -e '累計収入' -e '注意' /tmp/ros2mypkg.log || echo "ログが見つかりません。"
+grep -e '経過時間' -e '累計収入' -e '注意' /tmp/ros2mypkg.log
 
