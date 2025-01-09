6e34ece6f511aad99b9d9985b1158c01f485c312
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-10 05:17:10 +0900

    add

--- test/test.bash
+++ test/test.bash
@@ -7,16 +7,14 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-
-# ROS 2 のセットアップスクリプトをソース
-source /opt/ros/foxy/setup.bash
 source $dir/ros2_ws/install/setup.bash
 
-# ノードを実行し、ログを保存
-timeout 10 ros2 run ros2mypkg baito_publisher > /tmp/ros2mypkg.log &
+timeout 10 ros2 run ros2mypkg baito_publisher > /tmp/ros2mypkg.log &  
+
 
 sleep 2
 
-# ログファイルの内容を検索
-grep -e '経過時間' -e '累計収入' -e '注意' /tmp/ros2mypkg.log
+cat /tmp/mypkg.log | grep -e '経過時間': -e '累計収入'
 
+
+kill %1
