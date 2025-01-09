c92e079d83bed51ca0244ae94132a47756e97243
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-10 02:36:40 +0900

    add

--- test/test.bash
+++ test/test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2024 TAISEI SAKAI
+# SPDX-FileCopyrightText: 2025 TAISEI SAKAI
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
@@ -7,14 +7,10 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/ros2_ws/install/setup.bash
-
-timeout 10 ros2 run ros2mypkg baito_publisher > /tmp/ros2mypkg.log &  
+source $dir/.bashrc
+timeout 10 ros2 launch ros2mypkg baito_publisher > /tmp/ros2mypkg.log  
 
 
 sleep 2
 
-cat /tmp/ros2mypkg.log | grep -e '秒 とんかつバイト: ' -e '円'
-
-
-kill%1
+cat /tmp/ros2mypkg.log | grep -e '秒 : ' -e '円'
