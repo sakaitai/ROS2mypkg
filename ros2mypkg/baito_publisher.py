a8805bc8db38d304ad792781d5000a1cf52b49b7
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-10 05:22:09 +0900

    add

--- ros2mypkg/baito_publisher.py
+++ ros2mypkg/baito_publisher.py
@@ -15,23 +15,17 @@ class BaitoPublisher(Node):
         self.timer = self.create_timer(1.0, self.timer_callback)
 
         self.hourly_rate = 1300  # 時給
-        self.warning_threshold = 60 * 60 * 8  # 8時間で警告
 
     def timer_callback(self):
         elapsed_time = time.time() - self.start_time
         total_seconds = int(elapsed_time)
         earned_money = (total_seconds / 3600) * self.hourly_rate
 
-        warning_message = ""
-        if total_seconds > self.warning_threshold:
-            warning_message = "※注意: 8時間を超えました！休憩を取ってください。"
-
         # トピックに送信するメッセージ
         msg = String()
         msg.data = (
-            f'経過時間: {total_seconds}秒'
+            f'経過時間: {total_seconds}秒 '
             f'累計収入: {int(earned_money)}円'
-            f'{warning_message}'
         )
         self.publisher_.publish(msg)
 
@@ -48,3 +42,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
