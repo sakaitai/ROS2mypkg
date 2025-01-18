67fead77d5797a124dfc6183421d1f191aafb355
sakaitaisei <justdoit0430@i.softbank.jp>
2025-01-19 00:12:33 +0900

    add

--- ros2mypkg/baito_publisher.py
+++ ros2mypkg/baito_publisher.py
@@ -13,13 +13,12 @@ class BaitoPublisher(Node):
         self.publisher_ = self.create_publisher(Int32, 'baito_time', 10)  # トピック名
         self.start_time = time.time()
         self.timer = self.create_timer(1.0, self.timer_callback)
-
         self.hourly_rate = 1300  # 時給
 
     def timer_callback(self):
-        elapsed_time = int(time.time() - self.start_time)  # 経過秒数
+        elapsed_time = time.time() - self.start_time  # 経過秒数
         earned_money = int((elapsed_time / 3600) * self.hourly_rate)  # 累計収入（整数型）
-        self.publisher_.publish(Int32(data=earned_money))  # 累計収入のみを送信
+        self.publisher_.publish(Int32(data=earned_money))  # 累計収入を送信
 
 def main(args=None):
     rclpy.init(args=args)
